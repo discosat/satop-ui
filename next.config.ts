8cb7c0b28c1fd96621d21bc5f100dacc78711501
droggokid <raduflorinmunteanu@gmail.com>
2025-04-18 14:08:12 +0200

    fixed dev env after github pages

--- next.config.ts
+++ next.config.ts
@@ -6,34 +6,23 @@ export const allowedOrigins = [
   "wayf.wayf.dk",
 ];
 
+// detect CI
+const isCI = Boolean(process.env.GITHUB_ACTIONS);
+
 const nextConfig: NextConfig = {
-  /**
-   * Enable static exports.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: "export",
+  ...(isCI && {
+    output: "export",
+    basePath: "/nextjs-github-pages",
+    assetPrefix: "/nextjs-github-pages",
+  }),
 
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/nextjs-github-pages",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
   images: {
     unoptimized: true,
   },
-  /* config options here */
+
   experimental: {
     serverActions: {
-      allowedOrigins: allowedOrigins,
+      allowedOrigins,
     },
   },
 };
